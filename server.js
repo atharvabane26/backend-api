e7582b79c79f4bdd4ff5a4731906489054d2db20
Atharva Bane <baneatharva26@gmail.com>
2026-01-15 22:00:18 +0530

    fix auth route mounting

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.get("/", (req, res) => {
   res.send("API working");
 });
 
-app.use('/api/auth', require('./auth'));
+const authRoutes = require("./auth.js");
+
+console.log("AUTH ROUTES LOADED");
+
+app.use("/api/auth", authRoutes);
 
 
 const PORT = process.env.PORT || 5000;
